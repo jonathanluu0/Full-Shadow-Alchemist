8f69e9c6dde1665f6b14b5bd095e8864e78d9b8a
jonathanluu0 <jonathanluu0@gmail.com>
2024-07-23 23:38:54 -0700

    Fixed damageable enemies and projectile damage

--- Assets/Brendan Work/ProjectileDamage.cs
+++ Assets/Brendan Work/ProjectileDamage.cs
@@ -2,14 +2,15 @@ using UnityEngine;
 
 public class ProjectileDamage : MonoBehaviour
 {
-    public int damageAmount = 10; // Amount of damage this projectile deals
-
-    void OnCollisionEnter2D(Collision2D collision)
+    public int damageAmount; // Amount of damage this projectile deals
+    
+    void OnTriggerEnter2D(Collider2D collision)
     {
         // Check if the collided object has a health component
         var health = collision.gameObject.GetComponent<EnemyHealth>();
         if (health != null)
         {
+            
             // Deal damage to the object
             health.TakeDamage(damageAmount);
         }
